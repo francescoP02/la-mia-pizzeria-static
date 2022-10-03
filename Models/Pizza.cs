c91907ea9040908d09ee1a2ebdd1f14167cfc669
francescoP02 <file.partipilo@gmail.com>
2022-10-03 14:52:28 +0200

    Added Pizza Class, implemented Bootstrap and CSS and added pizzas List

---
+++ Models/Pizza.cs
@@ -0,0 +1,18 @@
+﻿namespace la_mia_pizzeria_static.Models
+{
+    public class Pizza
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Photo { get; set; }
+        public float Price { get; set; }
+
+        public Pizza(string name, string description, string photo, string price)
+        {
+            Name = name;
+            Description = description;
+            Photo = photo;
+            Price = float.Parse(price);
+        }
+    }
+}
